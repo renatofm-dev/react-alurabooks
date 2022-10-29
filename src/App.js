dfd3b655462f87aa7bde562758824083e9c3dca0
Renato Martins <rmartins6694@gmail.com>
2022-10-29 09:13:13 -0300

    add logo in App.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './img/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
+        <div className='logo'>
+            <img src={logo} alt='logo' />
+            <p><strong>Alura</strong>Books</p>
+        </div>
       </header>
     </div>
   );
