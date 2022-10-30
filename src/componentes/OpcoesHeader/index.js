7bfe27e0a8a33e3bab73d3439a5a286d53825b05
Renato Martins <rmartins6694@gmail.com>
2022-10-30 08:47:27 -0300

    add styled components in OpcoesHeader/index.js

--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,14 +1,29 @@
-import './estilo.css'
 
+import styled from 'styled-components';
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE',];
+
+const Opcoes = styled.ul`
+    display: flex;
+`
+const OpcoesItem = styled.li`
+    min-width: 120px;
+    font-size: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+    padding:  0 5px;
+    cursor: pointer;
+`
 
 function OpcoesHeader () {
     return (
-        <ul className='opcoes'>
+        <Opcoes className='opcoes'>
             { textoOpcoes.map((texto) => (
-            <li className='opcoes__item'><p>{texto}</p></li>
+            <OpcoesItem><p>{texto}</p></OpcoesItem>
             )) }
-      </ul>
+      </Opcoes>
 
     )
 }
