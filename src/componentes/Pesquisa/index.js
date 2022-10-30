37d2bb0847356edfc02767b5bbb7199d0b96b9aa
Renato Martins <rmartins6694@gmail.com>
2022-10-30 10:21:41 -0300

    add Input/index.js and Pesquisa/index.js

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,13 +1,38 @@
+import Input from "../Input";
+import styled from "styled-components";
+
+const PesquisaContainer = styled.section`
+    background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
+    color: #FFF;
+    text-align: center;
+    padding: 85px 0;
+    height: 270px;
+    width: 100%;
+`
+
+const Titulo = styled.h2 `
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
+`
+
+const SubTitulo = styled.h3`
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 40px;
+`
+
 
 function Pesquisa () {
     return (
-        <section>
-            <h2>Já sabe por onde começar?</h2>
-            <h3>Encontre seu livro em nossa estante.</h3>
-            <input 
-            placeholder="Escrevan sua próxima leitura"
+        <PesquisaContainer>
+            <Titulo>Já sabe por onde começar?</Titulo>
+            <SubTitulo>Encontre seu livro em nossa estante.</SubTitulo>
+            <Input 
+            placeholder="Escreva sua próxima leitura"
             />
-        </section>
+        </PesquisaContainer>
     )
 }
 
